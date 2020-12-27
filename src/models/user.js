4dc495f5b95be6e6ca8a476d95989fb52c67f58a
Gudo Fabi <godofredo.fabi@growsuper.com>
2020-12-27 23:21:32 +0800

    fix:

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose =  require('mongoose');
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,9 +40,38 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be greater than 0!')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
+// to hide the important fields like tokens and password of the user
+userSchema.methods.toJSON = function() {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.tokens
+    delete userObject.password
+
+    return userObject
+}
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisIsMyFuckingSecretCode')
+    
+    // Save the token of the user
+    user.tokens = user.tokens.concat({ token })
+    await user.save();
+
+    return token
+}
+
+// statics methods accessiable by model
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,28 +1,62 @@
 const express = require('express')
+const auth = require('../middleware/auth')
 const User = require('../models/user')
 const router = express.Router()
 
-// USERS
+// CREATE USER
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch (error) {
         res.status(400).send(error)
     }
 })
 
+// LOGIN
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({user, token})
     } catch (error) {
         res.status(400).send()
     }
 });
 
-router.get('/users', async (req, res) => {
+// LOGOUT USER
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+// LOGOUT ALL
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+// GET AUTHENTICATED USER
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+});
+
+//  GET ALL USERS
+router.get('/users', auth, async (req, res) => {
     try {
         const users = await User.find({})
         res.send(users);
@@ -31,38 +65,30 @@ router.get('/users', async (req, res) => {
     }
 });
 
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const user = await User.findById(_id)
-        !user ? res.status(404).send() : res.send(user)
-    } catch (error) {
-        res.status(500).send()
-    }
-});
-
-router.patch('/users/:id', async (req, res) => {
+// UPDATE
+router.patch('/users/me', auth, async (req, res) => {
+    // #INFO: "req.user" -- is came from the middleware/auth.js
     const updates = Object.keys(req.body)
-    console.log(updates)
     const allowedToUpdate = ['name', 'age', 'email', 'password']
     const isValid = updates.every(update => allowedToUpdate.includes(update))
     if(!isValid) {
         return res.status(400).send({error: 'Invalid updates!'})
     } 
     try {
-        const user = await User.findById(req.params.id)
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
-        !user ? res.status(404).send() : res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
     } catch (error) {
         res.status(400).send()
     }
 })
 
-router.delete('/users/:id', async (req, res) => {
+// DELETE
+router.delete('/users/me', auth, async (req, res) => {
+    // #INFO: "req.user" -- is came from the middleware/auth.js
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        !user ? res.status(404).send() : res.send(user)
+        await req.user.remove()
+        res.send(req.user)
     } catch (error) {
         res.status(500).send()
     }
