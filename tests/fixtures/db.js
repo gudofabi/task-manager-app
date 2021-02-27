df2819eba99a81fde37da8bd4a90aa25068f2a5e
Godofredo Fabi <godofredocpanel@gmail.com>
2021-02-28 00:21:42 +0800

    fix: finish test on tasks

--- tests/fixtures/db.js
+++ tests/fixtures/db.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const User = require('../../src/models/user')
+const Task = require('../../src/models/task')
 
 jest.useFakeTimers()
 
-const userOneId = mongoose.Types.ObjectId()
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
     _id: userOneId,
     name: 'Mike',
@@ -15,13 +16,54 @@ const userOne = {
     }]
 }
 
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+    _id: userTwoId,
+    name: 'Gudo',
+    email: 'steven@sample.com',
+    password: 's3cr3t!!!',
+    tokens: [{
+       token: jwt.sign({ _id: userTwoId }, process.env.JWT_SECRET) 
+    }]
+}
+
+const taskOne = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'This is first task',
+    completed: false,
+    owner: userOne._id
+}
+const taskTwo = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'This is second task',
+    completed: true,
+    owner: userOne._id
+}
+
+const taskThree = {
+    _id: new mongoose.Types.ObjectId(),
+    description: 'This is third task',
+    completed: true,
+    owner: userTwo._id
+}
+
 const setupDatabase = async () => {
     await User.deleteMany()
+    await Task.deleteMany()
     await new User(userOne).save()
+    await new User(userTwo).save()
+    await new Task(taskOne).save()
+    await new Task(taskTwo).save()
+    await new Task(taskThree).save()
 }
 
 module.exports = {
     userOneId,
     userOne,
+    userTwoId,
+    userTwo,
+    taskOne,
+    taskTwo,
+    taskThree,
     setupDatabase
 }
